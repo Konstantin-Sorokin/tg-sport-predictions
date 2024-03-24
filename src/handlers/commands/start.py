48d184310824a3854a37159625d45ee91d861b73
Konstantin Sorokin <61raven61@mail.ru>
2024-03-24 21:28:29 +0300

    Обновил команду start

--- src/handlers/commands/start.py
+++ src/handlers/commands/start.py
@@ -1,9 +1,14 @@
 from aiogram import Router, types
 from aiogram.filters import CommandStart
 
+from src.keyboards.user_keyboards.reply.main_menu_kb import get_main_menu_kb
+
 router = Router(name=__name__)
 
 
 @router.message(CommandStart())
 async def handle_start_command(message: types.Message):
-    await message.answer("Hello")
+    await message.answer(
+        text="Hello",
+        reply_markup=get_main_menu_kb(),
+    )
